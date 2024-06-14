52c6d332f35ef16f310f97a6bfe74bfd7013982d
veenacc <veena.chenicheri@gmail.com>
2024-06-14 16:06:16 -0400

    implemented user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/users" => "users#index"
+  # get "/users" => "users#index"
 
   get "/jobs" => "jobs#index"
   get "/jobs/:id" => "jobs#show"
+
+
+  post "/users" => "users#create"
 
 
 end
