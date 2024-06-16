6fc748a8edbe5e830ea84b6d42e3de5bc26da5ba
Alcy Torres <mxalcytorres@gmail.com>
2024-06-16 10:54:01 -0400

    Jobs add update action

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "/jobs" => "jobs#index"
   get "/jobs/:id" => "jobs#show"
   post "/jobs/" => "jobs#create"
+  patch "/jobs/:id" => "jobs#update"
 
 
   post "/users" => "users#create"
