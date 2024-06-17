5bc91f58a9f974a463eb610bfa942a2677d8e133
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-06-17 10:03:22 -0400

    Added to and modified seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,10 @@ job = Job.new(company_id: 101, title: "Teachers Assistant", description: "Help s
 job.save
 
 
-job = Job.new(company_id: 102, title: "data analyst", description: "teaches ruby programming on rails framework", url: "https://via.placeholder.com/150", location: "seattle", active: "TRUE", salary_range: "40k - 60k")
+job = Job.new(company_id: 102, title: "data analyst", description: "crunch data for information processing", url: "https://via.placeholder.com/150", location: "seattle", active: "TRUE", salary_range: "40k - 60k")
+job.save
+
+job = Job.new(company_id: 102, title: "prework instructor", description: "evaluate students prework", url: "https://via.placeholder.com/150", location: "Austin", active: "TRUE", salary_range: "40k-60k")
 job.save
 
 company1 = Company.new(name: "Actualize", logo: "https://anyonecanlearntocode.com/assets/actualize-logo-7cfc296a2f85a02cf29c8ee89707b1da416d72b05fe1d582c9eb9a2888d7dc18.png", description: "top US coding bootcamp")
